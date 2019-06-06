080eef9606dee33c0ed0bb1a700203cf179ab2df
AyMog <aymog4@gmail.com>
2019-06-06 12:27:07 -0400

    Project Creation

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Weather App
+      </header>
+      <main>
+        Main body
+      </main>
+    </div>
+  );
+}
+
+export default App;
