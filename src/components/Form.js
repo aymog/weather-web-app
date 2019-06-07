a52da1ff3f823f03bda493f7c4ed81ad536eb37f
AyMog <aymog4@gmail.com>
2019-06-07 01:09:26 -0400

    Styling edited

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,8 @@ export default class Form extends Component {
         return(
             <div>
                 <form onSubmit={this.props.getWeather}>
-                    <input className="Margin" style={{"box-shadow": "1px 1px 5px #000000"}} type="text" name="city" placeholder="City..."/>
-                    <input className="Margin" style={{"box-shadow": "1px 1px 5px #000000"}} type="text" name="country" placeholder="Country..."/>
+                    <input type="text" name="city" placeholder="City..."/>
+                    <input type="text" name="country" placeholder="Country..."/>
                     <button className="Margin" >Submit</button>
                 </form>
             </div>
